c795598d9a66c5259e5c310e51a593e052a6698c
EdinelsonBarros <edinelsonlimabarros@gmail.com>
2022-03-29 17:55:58 -0300

    Update

---
+++ Udemy/dev-web/inicio/bloco.js
@@ -0,0 +1,9 @@
+// a sentença de código pode terminar ou não com ;
+// um bloco é limitado pelo abrir e fechar de chaves {}
+// um bloco pode ter uma ou mais sentenças
+// um bloco pode ter outros blocos  dentro dele
+{
+    console.log('Passo #01')
+    console.log('Passo #02')
+    console.log('Passo #03')
+}
