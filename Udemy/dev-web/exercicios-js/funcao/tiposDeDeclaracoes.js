b047d027b41818fdbf57bbdc6881aa4d8ee03000
EdinelsonBarros <edinelsonlimabarros@gmail.com>
2022-04-12 13:57:52 -0300

    Update

---
+++ Udemy/dev-web/exercicios-js/funcao/tiposDeDeclaracoes.js
@@ -0,0 +1,19 @@
+console.log(soma(3, 4))
+
+// function declaration - desta foma é possivel o içamento
+function soma(x, y) {
+    return x + y
+}
+console.log(soma(3, 4))
+
+// function expression 
+const sub = function (x, y) {
+    return x - y
+}
+console.log(sub(3, 4))
+
+// named function expression
+const mult = function multi(x, y) {
+    return x * y
+}
+console.log(mult(3, 4))
