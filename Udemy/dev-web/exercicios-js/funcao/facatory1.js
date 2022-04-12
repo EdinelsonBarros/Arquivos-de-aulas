5eaac46e914de80f4bab898c2f0e71a20ec4b836
EdinelsonBarros <edinelsonlimabarros@gmail.com>
2022-04-12 18:06:09 -0300

    Update

---
+++ Udemy/dev-web/exercicios-js/funcao/facatory1.js
@@ -0,0 +1,9 @@
+// Factory simples
+function criarPessoa() {
+    return {
+        nome: 'Edinelson',
+        sobrenome: 'Barros',
+        endereco: 'rua 8 N 225'
+    }
+}
+console.log(criarPessoa())
