b8ad68ac81622d1b20f8b4f87f5c403f273848dc
EdinelsonBarros <edinelsonlimabarros@gmail.com>
2022-04-15 17:46:32 -0300

    Update

---
+++ Udemy/dev-web/exercicios-js/objeto/oo.js
@@ -0,0 +1,26 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+//PROCEDURAL
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento() // o foco passou a ser objeto e não a função. Na linguagem OO temos objetos que tem funções, enquanto na procedural temos funções que manipula dados.
+
+// Princípios importantes:
+
+// 1. abstraçao -> trazer caracteristicas importantes para o projeto, do mundo real para o código.
+
+// 2. encapsulamenton -> deixar os objetos encapsulados de forma que eles não interfiram uns nos outros mais do que o necessários.
+
+//3. Herança -> fazer obejtos maiores apartir de objetos menores. Priorize composição em vez da herança.
+
+//4. Poliformismo ->
