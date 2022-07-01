6fd2c422d0fa0fbda8f6af855155e6d79b5c2d1b
EdinelsonBarros <edinelsonlimabarros@gmail.com>
2022-06-30 21:53:46 -0300

    update

--- Udemy/dev-web/exercicios-js/node/moduloB.js
+++ Udemy/dev-web/exercicios-js/node/moduloB.js
@@ -1,3 +1,10 @@
+/*
+    Cada arquivo do node é um módulo
+    que está visível somente para ele mesmo.
+    Para que o arquivo esteja disponível fora
+    eu preciso usar 'module.exports' com no 
+    exemplo abaixo.
+*/
 module.exports = {
     bomDia: 'Bom dia!',
     boaNoite() {
--- Udemy/dev-web/exercicios-js/node/moduloCliente.js
+++ Udemy/dev-web/exercicios-js/node/moduloCliente.js
@@ -1,3 +1,9 @@
+/*
+    Para usar um módulo que está disponível 
+    globalmente é preciso usar a propriedade
+    'require' para requerer o modulo a ser 
+    utilizado.
+*/
 const moduloA = require('./moduloA')
 const moduloB = require('./moduloB')
 
