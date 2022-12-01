1a71e8da7aa3028b1fb266baff0601f0b2ac7ab6
EdinelsonBarros <edinelsonlimabarros@gmail.com>
2022-12-01 12:13:23 -0300

    Update

--- Udemy/dev-web/exercicios-js/vue/montyhall/src/App.vue
+++ Udemy/dev-web/exercicios-js/vue/montyhall/src/App.vue
@@ -1,16 +1,42 @@
 <template>
     <div id="app">
       <h1>Problema de Monty Hall</h1>
-      <GiftVue />
+      <div class="form">
+        <div v-if="!started">
+          <label for="portsAmount">Quantas portas?</label>
+          <input type="text" id="portsAmount" size="3"
+          v-model.number="portsAmount">
+        </div>
+        <div v-if="!started">
+          <label for="selectedPort">Qual porta é premiada?</label>
+          <input type="text" id="selectedPort" size="3"
+          v-model.number="selectedPort">
+        </div>
+        <button v-if="!started" @click="started = true">Iniciar</button>
+        <button v-if="started" @click="started = false">Reiniciar</button>
+      </div>
+      <div class="doors" v-if="started">
+        <div v-for="i in portsAmount" :key="i">
+          <DoorVue :hasGift="i === selectedPort" :number="i" />
+        </div>
+      </div>
     </div>
 
 </template>
 
 <script>
-import GiftVue from './components/GiftVue'
+import DoorVue from './components/DoorVue'
 export default {
   name: 'App',
-  components: { GiftVue }
+  components: { DoorVue }, 
+  data: function() {
+      return {
+        started: false,
+        portsAmount: 3,
+        selectedPort: null
+
+      }
+  }
 }
 </script>
 
@@ -30,5 +56,25 @@ body {
   background-color: #0004;
   padding: 20px;
   margin-bottom: 60px;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 40px;
+}
+
+.form, .form input, .form button {
+  margin-bottom: 10px;
+  font-size: 2rem;
+}
+
+.doors {
+  align-self: stretch;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
 }
 </style>
