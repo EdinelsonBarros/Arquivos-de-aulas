e829e0a5b437b29839eb80b962d433265ed23dd4
EdinelsonBarros <edinelsonlimabarros@gmail.com>
2023-02-07 19:28:03 -0300

    Criando api category

---
+++ Udemy/dev-web/exercicios-js/knowledge/versao-inicial/backend/api/category.js
@@ -0,0 +1,67 @@
+module.exports = app {
+    const { existsOrError, notExistsOrError  } = app.api.validation
+
+    const save = (req, res) => {
+        const category = { ...req.body }
+        if(req.params.id) category.id = req.params.id
+
+        try {
+            existsOrError(category.name, 'Nome não informado')
+        } catch(msg) {
+            return res.status(400).send(msg)
+        }
+        if(category.id) {
+            app.db('categories')
+                .update(category)
+                .where({ id: category.id })
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        } else {
+            app.db('categories')
+                .insert(category)
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        }
+    }
+
+    const remove = async (req, res) => {
+        try {
+            existsOrError(req.params.id, 'Código da categoria não foi informado.')
+
+            const subcategory = await app.db('categories')
+                .where({ parentId: req.params.id })
+
+            notExistsOrError(subcategory, 'Categoria possui subcategorias.')
+
+            const articles = await app.db('articles')
+                .where({ categoryId: req.params.id })
+            notExistsOrError(articles, 'Categorias possui artigos.')
+
+            const rowsDeleted = await app.db('categories')
+                .where({ id: req.params.id }).del()
+            existsOrError(rowsDeleted, 'Categoria não foi encontrada.')
+
+            res.status(204).send()
+        } catch {
+            res.status(400).send(msg)
+        }
+    }
+
+    const withPath = categories => {
+        const getParent = (categories, parentId) => {
+            const parent = categories.filter(parent => parent.id === parentId)
+            return parent.length ? parent[0] : null 
+        }
+
+        const categoriesWithPath = categories.map(category => {
+            let path = category.name
+            let parent = getParent(categories, categories.parentId)
+
+            while(parent) {
+                path = `${parent.name} > ${path}`
+                parent = getParent(categories, parent.parentId)
+            }
+            return { ...category }
+        })
+    }
+}
