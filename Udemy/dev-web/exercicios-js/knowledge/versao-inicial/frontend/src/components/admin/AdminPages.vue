321649a0b8110a551848375cf9d38e030b503c9b
EdinelsonBarros <edinelsonlimabarros@gmail.com>
2023-02-16 18:10:21 -0300

    Começado a aba usuários

--- Udemy/dev-web/exercicios-js/knowledge/versao-inicial/frontend/src/components/admin/AdminPages.vue
+++ Udemy/dev-web/exercicios-js/knowledge/versao-inicial/frontend/src/components/admin/AdminPages.vue
@@ -1,15 +1,33 @@
 <template>
     <div class="admin-pages">
         <PageTitle icon="fa fa-cogs" main="Administração do sistema" sub="Cadastros & Cia" />
+        <div class="admin-pages-tab">
+            <b-card no-bady>
+                <b-tabs card>
+                    <b-tab title="Artigos">
+                        <ArcticleAdmin />
+                    </b-tab>
+                    <b-tab title="Categorias">
+                       <CategoryAdmin />
+                    </b-tab>
+                    <b-tab title="Usuários" active>
+                        <UserAdmin />
+                    </b-tab>
+                </b-tabs>
+            </b-card>
+        </div>
     </div>
 </template>
 
 <script>
 import PageTitle from '../template/PageTitle'
+import ArcticleAdmin from './ArticleAdmin'
+import CategoryAdmin from './CategoryAdmin'
+import UserAdmin from './UserAdmin'
 
 export default {
     name: 'AdminPages',
-    components: { PageTitle }    
+    components: { PageTitle, ArcticleAdmin, CategoryAdmin, UserAdmin }    
 }
 </script>
 
--- Udemy/dev-web/exercicios-js/knowledge/versao-inicial/frontend/src/main.js
+++ Udemy/dev-web/exercicios-js/knowledge/versao-inicial/frontend/src/main.js
@@ -5,10 +5,15 @@ import Vue from 'vue'
 import App from './App'
 
 import './config/bootstrap'
+import './config/msgs'
 import store from './config/store'
 import router from './config/router'
 
 Vue.config.productionTip = false
+
+// TEMPORÁRIO!
+
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkVkaW5lbHNvbiBMaW1hIEJhcnJvcyIsImVtYWlsIjoiZWRpbmVsc29ubDk4QGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2NzY0ODYzNzgsImV4cCI6MTY3Njc0NTU3OH0.9nmzy7QOJqpRsJ0sOUW8zJz833KXg3cQGjLA-laiXVw'
 
 new Vue({
   store,
